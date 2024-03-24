2b6144757d853aa6f0d23ad67c3822e38c06c9e0
radenirsyad <89185652+radenirsyad@users.noreply.github.com>
2024-03-24 19:02:22 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-e06e5efd-d682-49ff-aedf-ee8dc2ac0478/"
 
 OUTPUT_DIR="logs"
 
